58110fbead5d275a23ddd0ec3fc7b068ae8cd9c0
Ошуркевич Роман Александрович <Oshurkevich.R.A@sberbank.ru>
2021-02-21 15:33:15 +0400

    finish table resize

--- webpack.config.js
+++ webpack.config.js
@@ -77,10 +77,6 @@ module.exports = {
         use: [
           {
             loader: MiniCssExtractPlugin.loader,
-            // options: {
-            //     hmr: isDev,
-            //     reloadAll: true
-            // }
           },
           'css-loader',
           'sass-loader',
